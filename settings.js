c335879f137d773bba754fdacd7678e638c867f0
Ge France Maasin <gefrancemaasin6@gmail.com>
2022-10-24 00:11:47 +0800

    Settings Page

---
+++ settings.js
@@ -0,0 +1,28 @@
+let popup = document.getElementById("popup");
+
+function openPopup() {
+    popup.classList.add("open-popup");
+}
+function closePopup() {
+    popup.classList.remove("open-popup");
+}
+
+
+const tabBtn =
+    document.querySelectorAll(".tab");
+const tab =
+    document.querySelectorAll(".tabShow");
+
+
+function tabs(panelIndex) {
+    tab.forEach(function (node) {
+        node.style.display = "none";
+    });
+    tab[panelIndex].style.display =
+        "block"
+}
+tabs(0);
+
+$(".tab").click(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+})
